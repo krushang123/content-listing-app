d5483982b2ac146e660dfb381137a394f4615b01
krushang patel <kp.krushang@gmail.com>
2024-04-10 11:43:46 +0530

    add fallback image

--- src/components/thumbnail/thumbnail-card.tsx
+++ src/components/thumbnail/thumbnail-card.tsx
@@ -1,29 +1,39 @@
 "use client"
 
+import { useState } from "react"
+
 import { type StaticImport } from "next/dist/shared/lib/get-img-props"
+import Image from "next/image"
 
 import { Stack, AspectRatio, Text } from "@chakra-ui/layout"
-import { Image } from "@chakra-ui/next-js"
 
 interface ThumbnailCardProps {
   src: string | StaticImport
   alt: string
   name: string
+  fallbackImageUrl: string
 }
 
 const ThumbnailCard = (props: ThumbnailCardProps) => {
-  const { src, alt, name } = props
+  const { src, alt, name, fallbackImageUrl } = props
+
+  const [isImageError, setIsImageError] = useState<boolean>(false)
+
+  const handleImageError = () => {
+    setIsImageError(true)
+  }
 
   return (
     <Stack>
       <AspectRatio ratio={2 / 3}>
         <Image
           priority
-          src={src}
+          src={isImageError ? fallbackImageUrl : src}
           alt={alt}
           fill
           sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-          sx={{
+          onError={handleImageError}
+          style={{
             objectFit: "cover",
             objectPosition: "center",
           }}
--- src/components/thumbnail/thumbnail-grid.tsx
+++ src/components/thumbnail/thumbnail-grid.tsx
@@ -14,15 +14,19 @@ interface ThumbnailGridProps {
 const ThumbnailGrid = (props: ThumbnailGridProps) => {
   const { posters } = props
 
+  const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL ?? "" // Retrieve the base image URL
+  const fallbackImageUrl = process.env.NEXT_PUBLIC_FALLBACK_IMAGE_URL ?? "" // Retrieve the fallback image URL
+
   return (
     <SimpleGrid columns={3} spacingX={6} spacingY={10}>
       {posters.map((item) => (
         <ThumbnailCard
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
           key={uuidv4()}
-          src={`https://test.create.diagnal.com/images/${item["poster-image"]}`}
+          src={`${imageUrl}${item["poster-image"]}`}
           alt={item.name}
           name={item.name}
+          fallbackImageUrl={fallbackImageUrl}
         />
       ))}
     </SimpleGrid>
