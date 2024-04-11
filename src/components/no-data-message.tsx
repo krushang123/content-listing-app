e2c1b2662a719a2f4d7ac464bb0142c2cb7de5cc
krushang patel <kp.krushang@gmail.com>
2024-04-11 08:41:27 +0530

    changes for no data found image

--- src/components/no-data-message.tsx
+++ src/components/no-data-message.tsx
@@ -4,6 +4,8 @@ import Image from "next/image"
 
 import { Text, Box, Stack } from "@chakra-ui/layout"
 
+import NoDataFoundImage from "../../public/images/no-data-found.jpg"
+
 interface NoDataMessageProps {
   messageTitle: string
   imageAlt?: string
@@ -16,9 +18,10 @@ const NoDataMessage = (props: NoDataMessageProps) => {
     <Stack align='center'>
       <Box w='250px' h='300px' position='relative'>
         <Image
-          src='/images/no-data-found.jpg'
+          src={NoDataFoundImage}
           alt={imageAlt}
           fill
+          placeholder='blur'
           sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
           style={{
             objectFit: "cover",
